af0fdb73ec66adc5f0cdf1c61a5bd1edd7cefcca
Masuma Shariff <61000672+masstar786@users.noreply.github.com>
2021-01-13 20:54:01 +0300

    Add files via upload

---
+++ fname-lname-simplifier.py
@@ -0,0 +1,15 @@
+# Code for the users first and last name
+# Ask for first name
+print ("What is your first name?")
+fname = input("Insert your first name: ")
+
+# Ask for last name
+print ("What is your last name?")
+lname = input("Insert your last name: ")
+
+# Execute the code and display full last and first name
+print("Is your name,", fname.title(), lname.title())
+
+#for printing initials;
+print ("Your initials are,", fname.upper()[0], lname.upper()[0])
+
